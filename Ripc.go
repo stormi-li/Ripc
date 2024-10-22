9d809e7b190d3e55dd42f517561ea69889db041c
Your Name <you@example.com>
2024-10-23 00:50:38 +0800

    v1.0.0

---
+++ Ripc.go
@@ -0,0 +1,80 @@
+package ripc
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Ripc结构体
+type Client struct {
+	RedisClient *redis.Client
+}
+
+// 创建Ripc客户端
+func NewClient(addr string) (*Client, error) {
+	client := Client{}
+	rdsC := redis.NewClient(&redis.Options{
+		Addr: addr,
+	})
+
+	// 测试连接
+	if err := rdsC.Ping(context.Background()).Err(); err != nil {
+		return nil, fmt.Errorf("failed to connect to Redis at %s", addr) // 返回错误
+	}
+
+	client.RedisClient = rdsC
+	return &client, nil
+}
+
+// 向所有监听管道的进程发送通知
+func (client Client) Notify(ctx context.Context, channel, msg string) {
+	client.RedisClient.Publish(ctx, channel, msg)
+}
+
+// 监听一个消息，返回收到的信息，如果超时返回""
+func (client Client) Wait(ctx context.Context, channel string, timeout time.Duration) string {
+	sub := client.RedisClient.Subscribe(ctx, channel)
+	msg := sub.Channel()
+	timer := time.NewTicker(timeout)
+	defer timer.Stop()
+	defer sub.Close()
+	select {
+	case <-timer.C:
+		return ""
+	case msg := <-msg:
+		return msg.Payload
+	}
+}
+
+type Listener struct {
+	shutdown chan struct{}
+	sub      *redis.PubSub
+}
+
+func (listener Listener) Close() {
+	listener.shutdown <- struct{}{}
+}
+
+func (client *Client) NewListener(ctx context.Context, channel string) *Listener {
+	listener := Listener{}
+	listener.sub = client.RedisClient.Subscribe(ctx, channel)
+	listener.shutdown = make(chan struct{},1)
+	return &listener
+}
+
+// 接受所有发送过来的消息，并执行handler
+func (listener *Listener) Listen(handler func(msg string)) {
+	c := listener.sub.Channel()
+	defer listener.sub.Close()
+	for {
+		select {
+		case msg := <-c:
+			handler(msg.Payload)
+		case <-listener.shutdown:
+			return
+		}
+	}
+}
---
+++ test/test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	ripc "github.com/stormi-li/Ripc"
+)
+
+func main() {
+	client, err := ripc.NewClient("localhost:6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	listener := client.NewListener(context.Background(), "c1")
+	go func() {
+		listener.Listen(func(msg string) {
+			fmt.Println(msg)
+			if msg == "4" {
+				listener.Close()
+			}
+			time.Sleep(500 * time.Millisecond)
+		})
+		fmt.Println("hhhh")
+	}()
+	go func() {
+		fmt.Println(client.Wait(context.Background(), "c1", 200*time.Millisecond))
+	}()
+	time.Sleep(100 * time.Millisecond)
+	go func() {
+		client.Notify(context.Background(), "c1", "1")
+		client.Notify(context.Background(), "c1", "2")
+		client.Notify(context.Background(), "c1", "3")
+		client.Notify(context.Background(), "c1", "4")
+	}()
+
+	select {}
+}
